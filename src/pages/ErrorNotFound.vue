c18e47236df6166ed56e160cdb682267c345aba8
piskesocute <happyday9837@gmail.com>
2023-12-28 05:46:43 +0800

    refactor: 單純改成script setup

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,13 +1,18 @@
-<template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+<script>
+import { defineComponent } from 'vue';
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
+export default defineComponent({
+  name: 'ErrorNotFound'
+});
+</script>
+<template>
+  <div
+    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
+  >
+    <div>
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
 
       <q-btn
         class="q-mt-xl"
@@ -21,11 +26,3 @@
     </div>
   </div>
 </template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'ErrorNotFound'
-})
-</script>
