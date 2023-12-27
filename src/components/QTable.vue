1a7ea651b6532bc15568ac05828d892f71e7e9d2
piskesocute <happyday9837@gmail.com>
2023-12-28 05:48:25 +0800

    feat: API資料表格
    API資料表格的呈現，因為只有第一筆資料會是從API抓取下來，其餘的都用local

--- src/components/QTable.vue
+++ src/components/QTable.vue
@@ -1,42 +1,91 @@
+<script setup>
+import { ref, watch, defineEmits, defineProps } from 'vue';
+
+const props = defineProps({
+  columns: {
+    type: Array,
+    default: () => []
+  },
+
+  rows: {
+    type: Array,
+    default: () => []
+  }
+});
+
+const emit = defineEmits(['update:listData']);
+
+const filter = ref('');
+
+const selected = ref([]);
+
+watch(
+  () => props.rows,
+  (val) => {
+    selected.value = [];
+  }
+);
+
+watch(
+  () => selected.value,
+  (val) => {
+    emit('update:listData', val);
+  }
+);
+</script>
+
 <template>
   <q-table
+    selection="multiple"
+    v-model:selected="selected"
     class="q-mt-sm"
-    row-key="name"
+    row-key="email"
     :columns="columns"
     :rows="rows"
     separator="cell"
     flat
     bordered
-  />
+    :filter="filter"
+    no-data-label="找不到資料，請重新整理"
+    no-results-label="搜尋不到資料，請重新輸入"
+  >
+    <template v-slot:top-right>
+      <q-input
+        borderless
+        dense
+        debounce="300"
+        v-model="filter"
+        placeholder="請輸入你希望搜尋的欄位"
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </template>
+    <template v-slot:header-selection="props">
+      <q-checkbox v-model="props.selected" />
+    </template>
+
+    <template v-slot:body="props">
+      <q-tr :props="props">
+        <q-td>
+          <q-checkbox v-model="props.selected" />
+        </q-td>
+
+        <q-td v-for="col in props.cols" :key="col.name" :props="props">
+          <q-tooltip
+            anchor="bottom middle"
+            self="center middle"
+            transition-show="scale"
+            transition-hide="scale"
+          >
+            {{ col.value }}
+          </q-tooltip>
+          {{ col.value }}
+        </q-td>
+      </q-tr>
+    </template>
+  </q-table>
 </template>
-<script>
-import { onMounted } from 'vue';
 
-export default {
-  props: {
-    columns: {
-      type: Array,
-      default: () => [],
-    },
-
-    rows: {
-      type: Array,
-      default: () => [],
-    },
-  },
-
-  setup(props, { emit }) {
-
-    onMounted(() => {
-
-    });
-
-    return {
-
-    };
-  },
-};
-</script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
